dd3099a9e8bb0ce45aa070e8d44218aed0771362
Wojciech Olejnik <divoolej@Divoolej-MacbookPro.local>
2016-01-21 22:49:23 +0100

    Add a simple User model

---
+++ app/models/user.rb
@@ -0,0 +1,9 @@
+class User < ActiveRecord::Base
+  has_secure_password
+
+  validates :first_name, presence: true
+  validates :last_name,  presence: true
+  validates :username,   presence: true
+  validates :email,      presence: true, uniqueness: { case_sensitive: false }
+  validates :password,   presence: true, length: { minimum: 8 }
+end
