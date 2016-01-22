dda7a22f2ccd76a33cd4e6ac0cf849006c4e5b05
Wojciech Olejnik <divoolej@gmail.com>
2016-01-22 07:37:22 +0100

    Add a route for authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,15 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
+      # USERS
       get    '/users'    => 'users#index'
       post   '/users'    => 'users#create'
       get    '/user/:id' => 'users#show'
       put    '/user/:id' => 'users#update'
       delete '/user/:id' => 'users#destroy'
+
+      # SESSIONS
+      post   '/sign_in'  => 'authentications#create'
     end
   end
 end
