308aa7ad98a175635352650168959d0809baf491
Wojciech Olejnik <divoolej@gmail.com>
2016-01-22 07:36:23 +0100

    Add #create to AuthenticationsController

--- app/controllers/api/v1/authentications_controller.rb
+++ app/controllers/api/v1/authentications_controller.rb
@@ -1,4 +1,12 @@
 class Api::V1::AuthenticationsController < Api::V1::BaseController
   def create
+    user = User.find_by! email: params[:email]
+    if user.authenticate params[:password]
+      render json: {
+        authentication_token: user.authentication_token,
+        user: Api::V1::UserSerializer.new(user).to_json
+      }
+    end
+    return api_error status: 403, errors: 'Unauthenticated'
   end
 end
