828fb77f878ee43997e86f469a2110c90a3d93b2
Wojciech Olejnik <divoolej@gmail.com>
2016-01-22 19:39:11 +0100

    Fix typo

--- app/controllers/api/v1/base_controller.rb
+++ app/controllers/api/v1/base_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::BaseController < ApplicationController
   before_action :destroy_session
-  rescue_from ActiveRecord::RecordNotFound, with: not_found
+  rescue_from ActiveRecord::RecordNotFound, with: :not_found
 
   def destroy_session
     request.session_options[:skip] = true
