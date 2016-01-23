50e89bd64d64648c12ae208be59120a20e88bc39
Wojciech Olejnik <divoolej@gmail.com>
2016-01-23 23:18:40 +0100

    Add #index to BragsController

--- app/controllers/api/v1/brags_controller.rb
+++ app/controllers/api/v1/brags_controller.rb
@@ -1,6 +1,17 @@
 class Api::V1::BragsController < Api::V1::BaseController
 
   def index
+    user = User.find(params[:id])
+    if @current_user == user
+      brags = user.brags
+      render json: ActiveModel::ArraySerializer.new(
+        brags,
+        each_serializer: Api::V1::BragSerializer,
+        root: :brags,
+      )
+    else
+      render json: { errors: 'Unauthorized' }, status: 403
+    end
   end
 
   def show
