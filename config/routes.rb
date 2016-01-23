113f7bd2b1a7cad5c718726a188c0952c6ad5e08
Wojciech Olejnik <divoolej@gmail.com>
2016-01-23 19:45:01 +0100

    Change the route for users#show to /user/:username

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,11 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       # USERS
-      get    '/users'    => 'users#index'
-      post   '/users'    => 'users#create'
-      get    '/user/:id' => 'users#show'
-      put    '/user/:id' => 'users#update'
-      delete '/user/:id' => 'users#destroy'
+      get    '/users'          => 'users#index'
+      post   '/users'          => 'users#create'
+      get    '/user/:username' => 'users#show'
+      put    '/user/:id'       => 'users#update'
+      delete '/user/:id'       => 'users#destroy'
 
       # SESSIONS
       post   '/sign_in'  => 'authentications#create'
