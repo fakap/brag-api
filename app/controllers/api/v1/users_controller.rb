a39a563a2eee23e8188d0e95e61ea16069cb9c52
Wojciech Olejnik <divoolej@gmail.com>
2016-01-23 19:51:03 +0100

    Add #index action to UsersController

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -2,6 +2,12 @@ class Api::V1::UsersController < Api::V1::BaseController
   skip_before_action :authenticate!, only: [:create]
 
   def index
+    users = User.all
+    render json: ActiveModel::ArraySerializer.new(
+      users,
+      each_serializer: Api::V1::UserSerializer,
+      root: :users,
+    )
   end
 
   def show
