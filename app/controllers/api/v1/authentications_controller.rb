dfe87f458f43bce147ba24f3770fce026d5be422
Wojciech Olejnik <divoolej@gmail.com>
2016-01-23 19:49:30 +0100

    Don't return 404 errors when trying to fetch the authentication_token

--- app/controllers/api/v1/authentications_controller.rb
+++ app/controllers/api/v1/authentications_controller.rb
@@ -2,8 +2,8 @@ class Api::V1::AuthenticationsController < Api::V1::BaseController
   skip_before_action :authenticate!, only: [:create]
 
   def create
-    user = User.find_by! email: params[:email]
-    if user.authenticate params[:password]
+    user = User.find_by(email: params[:email])
+    if user && user.authenticate params[:password]
       render json: {
         authentication_token: user.authentication_token,
         user: Api::V1::UserSerializer.new(user)
