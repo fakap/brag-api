90bd240167dcda0d7d3cbe80516313efcdd0be48
Wojciech Olejnik <divoolej@gmail.com>
2016-01-23 19:42:45 +0100

    Remove email from UserSerializer

--- app/serializers/api/v1/user_serializer.rb
+++ app/serializers/api/v1/user_serializer.rb
@@ -1,3 +1,3 @@
 class Api::V1::UserSerializer < Api::V1::BaseSerializer
-  attributes :first_name, :last_name, :username, :email, :created_at
+  attributes :first_name, :last_name, :username, :created_at
 end
