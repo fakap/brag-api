7e038fe09f9615bcf51f4d69a21237c300eaf68b
Wojciech Olejnik <divoolej@gmail.com>
2016-01-23 23:19:48 +0100

    Add #update to BragsController

--- app/controllers/api/v1/brags_controller.rb
+++ app/controllers/api/v1/brags_controller.rb
@@ -34,6 +34,18 @@ class Api::V1::BragsController < Api::V1::BaseController
   end
 
   def update
+    brag = Brag.find(params[:id])
+    if @current_user == brag.user
+      brag_update_service = UpdateBragService.new
+      brag = brag_update_service.call
+      if brag.valid?
+        render json: Api::V1::BragSerializer.new(brag)
+      else
+        render json: { errors: brag.errors.messages }, status: 400
+      end
+    else
+      render json: { errors: 'Unauthorized' }, status: 403
+    end
   end
 
   def destroy
