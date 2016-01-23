b720c1d04017c06533a192e84b11abbd3620b174
Wojciech Olejnik <divoolej@gmail.com>
2016-01-23 19:46:38 +0100

    Add :authenticate! filter to Api::V1::BaseController
    This filter authenticates a user with using the  authentication_token

--- app/controllers/api/v1/base_controller.rb
+++ app/controllers/api/v1/base_controller.rb
@@ -1,12 +1,27 @@
 class Api::V1::BaseController < ApplicationController
   before_action :destroy_session
+  before_action :authenticate!
+
   rescue_from ActiveRecord::RecordNotFound, with: :not_found
 
-  def destroy_session
-    request.session_options[:skip] = true
-  end
+  private
 
-  def not_found
-    render json: { error: 'Not found' }, status: 404
-  end
+    def authenticate!
+      token, options = ActionController::HttpAuthentication::Token.token_and_options(request)
+      if token && options
+        user = User.find_by(email: options[:email])
+        if user && ActiveSupport::SecurityUtils.secure_compare(user.authentication_token, token)
+          return @current_user = user
+        end
+      end
+      render json: { error: 'Unauthenticated' }, status: 401
+    end
+
+    def destroy_session
+      request.session_options[:skip] = true
+    end
+
+    def not_found
+      render json: { error: 'Not found' }, status: 404
+    end
 end
