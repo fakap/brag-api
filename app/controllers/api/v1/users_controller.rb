d68c8edf253881118106e5766e46307be4f9fb27
Wojciech Olejnik <divoolej@gmail.com>
2016-01-23 19:51:11 +0100

    Add #show action to UsersController

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -11,6 +11,8 @@ class Api::V1::UsersController < Api::V1::BaseController
   end
 
   def show
+    user = User.find_by! username: params[:username]
+    render json: Api::V1::UserSerializer.new(user)
   end
 
   def create
