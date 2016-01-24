7af80a5c5a5d29ccfd0eea7ad9040586363e0d44
Wojciech Olejnik <divoolej@gmail.com>
2016-01-24 20:13:32 +0100

    Move the Api::V1::BragSerializer to the correct place

--- app/serializers/api/v1/brag_serializer.rb
+++ app/serializers/api/v1/brag_serializer.rb
@@ -1,3 +1,3 @@
 class Api::V1::BragSerializer < Api::V1::BaseSerializer
-  attributes :user_id, :title, :story
+  attributes :id, :user_id, :title, :story, :photo
 end
--- app/serializers/brag_serializer.rb
+++
@@ -1,3 +0,0 @@
-class Api::V1::BragSerializer < Api::V1::BaseSerializer
-  attributes :id, :user_id, :title, :story
-end
