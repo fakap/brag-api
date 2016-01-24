f5062f01a1587e9bce0080ff28e1e29b1f02ad86
Wojciech Olejnik <divoolej@gmail.com>
2016-01-24 20:38:42 +0100

    Secure uploaded files

--- app/uploaders/brag_uploader.rb
+++ app/uploaders/brag_uploader.rb
@@ -13,7 +13,7 @@ class BragUploader < CarrierWave::Uploader::Base
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
-    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+    "brags/#{Digest::SHA256.hexdigest(model.user_id.to_s)}"
   end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
@@ -44,8 +44,8 @@ class BragUploader < CarrierWave::Uploader::Base
 
   # Override the filename of the uploaded files:
   # Avoid using model.id or version_name here, see uploader/store.rb for details.
-  # def filename
-  #   "something.jpg" if original_filename
-  # end
+  def filename
+    "#{SecureRandom.uuid}.#{file.extension}" if original_filename
+  end
 
 end
