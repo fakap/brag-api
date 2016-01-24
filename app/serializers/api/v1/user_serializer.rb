9b4781e174814ef608634088c7314f74dd7eb8c9
Wojciech Olejnik <divoolej@gmail.com>
2016-01-24 19:23:54 +0100

    Add :id field to UserSerializer

--- app/serializers/api/v1/user_serializer.rb
+++ app/serializers/api/v1/user_serializer.rb
@@ -1,3 +1,3 @@
 class Api::V1::UserSerializer < Api::V1::BaseSerializer
-  attributes :first_name, :last_name, :username, :created_at
+  attributes :id, :first_name, :last_name, :username, :created_at
 end
