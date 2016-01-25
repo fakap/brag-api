0f3f0030772cec0830a94143750d79e4d89432e7
Wojciech Olejnik <divoolej@gmail.com>
2016-01-25 22:48:32 +0100

    Update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160124083444) do
+ActiveRecord::Schema.define(version: 20160125212533) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,6 +25,14 @@ ActiveRecord::Schema.define(version: 20160124083444) do
     t.string   "photo"
   end
 
+  create_table "comments", force: :cascade do |t|
+    t.integer  "user_id"
+    t.integer  "brag_id"
+    t.string   "text"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string   "first_name"
     t.string   "last_name"
@@ -34,6 +42,13 @@ ActiveRecord::Schema.define(version: 20160124083444) do
     t.datetime "created_at",           null: false
     t.datetime "updated_at",           null: false
     t.string   "authentication_token"
+  end
+
+  create_table "wows", force: :cascade do |t|
+    t.integer  "user_id"
+    t.integer  "brag_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
