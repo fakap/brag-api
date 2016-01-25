3887b86ac022a5761536b366ca0bbfe6b72e0d87
Wojciech Olejnik <divoolej@gmail.com>
2016-01-25 22:44:36 +0100

    Add routes for comments

--- config/routes.rb
+++ config/routes.rb
@@ -9,18 +9,23 @@ Rails.application.routes.draw do
       delete '/user/:id'       => 'users#destroy'
 
       # SESSIONS
-      post   '/sign_in'  => 'authentications#create'
+      post   '/sign_in' => 'authentications#create'
 
       # BRAGS
-      get    '/user/:id/brags'  => 'brags#index'
-      post   '/user/:id/brags'  => 'brags#create'
-      get    '/brag/:id'        => 'brags#show'
-      put    '/brag/:id'        => 'brags#update'
-      delete '/brag/:id'        => 'brags#destroy'
+      get    '/user/:id/brags' => 'brags#index'
+      post   '/user/:id/brags' => 'brags#create'
+      get    '/brag/:id'       => 'brags#show'
+      put    '/brag/:id'       => 'brags#update'
+      delete '/brag/:id'       => 'brags#destroy'
 
       # WOWS
-      get    '/brag/:id/wows'   => 'wows#index'
-      put    '/brag/:id/wow'    => 'wows#update'
+      get    '/brag/:id/wows' => 'wows#index'
+      put    '/brag/:id/wow'  => 'wows#update'
+
+      # COMMENTS
+      get    '/brag/:id/comments' => 'comments#index'
+      post   '/brag/:id/comments' => 'comments#create'
+      delete '/comment/:id'       => 'comment#destroy'
     end
   end
 end
